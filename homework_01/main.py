8e756ba8958f332ecab789ea3c78445c85245521
mlukashov <menguk@mail.ru>
2023-06-18 20:12:49 +0300

    Add solution for filter_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [number ** 2 for number in numbers]
 
 
 # filter types
@@ -18,8 +19,15 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def  is_prime(number):
+    if number <= 1:
+        return None # check that number valid
+    item = 2
+    while (number % item) != 0:
+            item += 1
+    return item == number
 
-def filter_numbers():
+def filter_numbers(number_list, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +38,9 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if filter_type == ODD:
+        return [number for number in number_list if (number % 2 != 0)]
+    if filter_type == EVEN:
+        return [number for number in number_list if (number % 2 == 0)]
+    if filter_type == PRIME:
+        return [number for number in number_list if (is_prime(number))]
